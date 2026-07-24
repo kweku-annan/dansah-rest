5535362418ae52fb95ee1445c19e0c72547d8533
kweku-annan <kwekuannan.dev@gmail.com>
2026-07-24 02:11:42 +0000

    feat: add healthcheck endpoint

--- dansah/urls.py
+++ dansah/urls.py
@@ -18,12 +18,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 
 admin.site.site_header = "Dansah Admin"
 # Add the below line
 admin.site.index_title = "Dansah  App"
 
+
+def health_check(request):
+    return JsonResponse({"status": "ok"}, status=200)
+
 urlpatterns = [
+    path("health/", health_check),
     path("admin/", admin.site.urls),
     path("api/homeslider/", include("homeslider.urls")),
     path("api/homeactivities/", include("homeactivities.urls")),
